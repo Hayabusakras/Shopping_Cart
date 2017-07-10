9ba1a4ef29773f74a8cef657a554811a0966be06
Hayabusakras <Hayabusakras@gmail.com>
2017-07-10 14:46:43 +0300

    stripe payments view

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,14 @@ Route::get('/shopping-cart/', [
     'uses' => 'ProductController@getCart',
     'as' => 'product.shoppingCart'
 ]);
+Route::get('/checkout', [
+   'uses' => 'ProductController@getCheckout',
+    'as' => 'checkout'
+]);
+Route::post('/checkout', [
+    'uses' => 'ProductController@postCheckout',
+    'as' => 'checkout'
+]);
 Route::get('/signup', [
     'uses' => 'UserController@getSignup',
     'as' => 'user.signup'
