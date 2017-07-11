d8fcef10bc17786644568db9c065fbd396a02e0a
Hayabusakras <Hayabusakras@gmail.com>
2017-07-11 15:17:23 +0300

    save orders in the database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Cart;
 use Illuminate\Http\Request;
 use App\Product;
+use App\Order;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Mockery\CountValidator\Exception;
 use Stripe\Stripe;
@@ -56,12 +58,19 @@ class ProductController extends Controller
         $cart = new Cart($oldCart);
         Stripe::setApiKey('sk_test_qF9VSIw8QOOIXyGF6KQGlMyW');
         try {
-            Charge::create(array(
+            $charge = Charge::create(array(
                 "amount" => $cart->totalPrice*100,
                 "currency" => "usd",
                 "source" => $request->input('stripeToken'), // obtained with Stripe.js
                 "description" => "Test Charge"
             ));
+            $order = new Order();
+            $order->cart = serialize($cart);
+            $order->address = $request->input('address');
+            $order->name = $request->input('name');
+            $order->payment_id = $charge->id;
+
+            Auth::user()->orders()->save($order);
         }
         catch(\Exception $e) {
             return redirect()->route('checkout')->with('error', $e->getMessage());
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,6 +16,7 @@
     @yield('content')
 </div>
 
+
 <script
         src="https://code.jquery.com/jquery-3.2.1.min.js"
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
